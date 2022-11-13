5d06fad7f425640435198412bf3ef4ccaf841646
Pratik Anand <anandnidokidos@gmail.com>
2022-11-14 01:54:11 +0530

    Integrate firebase, user creation

---
+++ infra/firebase/clientApp.ts
@@ -0,0 +1,33 @@
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// if (!firebase.getApps().length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+// export default firebase;
+
+// if (!getApps().length) {
+//   initializeApp(firebaseConfig);
+// }
+
+const app = initializeApp(firebaseConfig);
+const analytics = undefined;
+const auth = getAuth(app);
+
+export { app, analytics, auth };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+
 import Head from 'next/head';
 import Image from 'next/image';
+
+
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { auth } from '../infra/firebase/clientApp';
 
 import { Button } from 'baseui/button';
 
 export default function Home() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const onCreateUser = async () => {
+    try {
+      if (email && password) {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredentials.user;
+      }
+    } catch (error: any) {
+
+    };
+
+  };
+
   return (
     <div>
-      <Button>Hello world</Button>
+      <Button onClick={onCreateUser}>Hello world</Button>
     </div>
   )
 };
